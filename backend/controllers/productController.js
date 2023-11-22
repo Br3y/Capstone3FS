008b622792b23f09d5fdf93d7d9a11cd586f766a
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-11-23 07:17:39 +0800

    filter products in backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -181,17 +181,30 @@ const fetchTopProducts = asyncHandler(async (req, res) => {
 });
 
 const fetchNewProducts = asyncHandler(async (req, res) => {
-    try{
+  try {
+    const products = await Product.find().sort({ _id: -1 }).limit(5);
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+});
 
-        const products = await Product.find().sort({_id: -1}).limit(5)
-        res.json(products)
+const filterProducts = asyncHandler(async (req, res) => {
+  try {
+    const { checked, radio } = req.body;
 
-    } catch(error){
-        console.log(error)
-        res.status(400)
-        .json(error.message)
-    }
-})
+    let args = {};
+    if (checked.length > 0) args.category = checked;
+    if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
+
+    const products = await Product.find(args);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
 export {
   addProduct,
@@ -202,5 +215,6 @@ export {
   fetchAllProducts,
   addProductReview,
   fetchTopProducts,
-  fetchNewProducts
+  fetchNewProducts,
+  filterProducts,
 };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,47 +1,39 @@
 import express from "express";
-import formidable from 'express-formidable'
-const router = express.Router()
+import formidable from "express-formidable";
+const router = express.Router();
 
 import {
-    addProduct,
-    updateProductDetails,
-    removeProduct,
-    getAllProducts,
-    getProductById,
-    fetchAllProducts,
-    addProductReview,
-    fetchTopProducts,
-    fetchNewProducts
-} from '../controllers/productController.js'
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  getAllProducts,
+  getProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProducts,
+  fetchNewProducts,
+  filterProducts,
+} from "../controllers/productController.js";
 import { authenticate, authorizeAdmin } from "../middleware/authMiddleWare.js";
-import checkId from '../middleware/checkId.js'
+import checkId from "../middleware/checkId.js";
+
+router.route("/fetchAllProducts").get(fetchAllProducts);
+
+router.route("/:id/reviews").post(authenticate, checkId, addProductReview);
+
+router.route("/top").get(fetchTopProducts);
+router.route("/new").get(fetchNewProducts);
 
 router
-    .route('/fetchAllProducts')
-    .get(fetchAllProducts)
-
+  .route("/")
+  .post(authenticate, authorizeAdmin, formidable(), addProduct)
+  .get(getAllProducts);
 router
-    .route('/:id/reviews')
-    .post(authenticate, checkId, addProductReview)
+  .route("/:id")
+  .get(getProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, removeProduct);
 
-router
-    .route('/top')
-    .get(fetchTopProducts)
-router
-    .route('/new')
-    .get(fetchNewProducts)
+router.route("/filtered-products").post(filterProducts);
 
-router
-    .route('/')
-    .post(authenticate, authorizeAdmin, formidable(), addProduct)
-    .get(getAllProducts)
-router
-    .route('/:id')
-    .get(getProductById)
-    .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
-    .delete(authenticate, authorizeAdmin, removeProduct)
-
-
-
-
-export default router
+export default router;
