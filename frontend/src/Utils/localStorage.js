e495eb53d8fbbf5b71fbc77e901cd54ab6c6d300
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-11-23 03:26:30 +0800

    feat: able to add favorites products to localstorage and toggle it

---
+++ frontend/src/Utils/localStorage.js
@@ -0,0 +1,23 @@
+// Add a product to a localStorage
+export const addFavoritesToLocalStorage = (product) => {
+  const favorites = getFavoritesFromLocalStorage();
+  if (!favorites.some((p) => p._id === product._id)) {
+    favorites.push(product);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }
+};
+
+// Remove
+export const removeFavoritesFromLocalStorage = (productId) => {
+  const favorites = getFavoritesFromLocalStorage();
+  const updateFavorites = favorites.filter(
+    (product) => product._id !== productId
+  );
+  localStorage.setItem("favorites", JSON.stringify(updateFavorites));
+};
+
+// Retrieve
+export const getFavoritesFromLocalStorage = () => {
+  const favoritesJSON = localStorage.getItem("favorites");
+  return favoritesJSON ? JSON.parse(favoritesJSON) : [];
+};
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -10,11 +10,20 @@ import { apiSlice } from "./api/apiSlice";
 // Importing the authReducer from the authSlice file
 import authReducer from "./features/auth/authSlice";
 
+import favoritesReducer from "../redux/features/favorites/favoriteSlice";
+import { getFavoritesFromLocalStorage } from "../Utils/localStorage";
+const initialFavorites = getFavoritesFromLocalStorage() || [];
+
 // Creating the Redux store with the configured reducers, middleware, and devTools settings
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer, // Including the API slice reducer under a specific key
     auth: authReducer, // Including the authReducer for handling authentication state
+    favorites: favoritesReducer,
+  },
+
+  preloadedState: {
+    favorites: initialFavorites,
   },
 
   // Configuring middleware to include the API middleware along with the default middleware
