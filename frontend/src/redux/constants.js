766503b4f0ad7191102454c2f401fbb78c223602
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-11-22 14:02:04 +0800

    feat: frontend Product setup API to connect backend database

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = " "; //proxy
 export const USERS_URL = "/b6/users";
 export const CATEGORY_URL = '/b6/category'
+export const PRODUCT_URL = '/b6/products'
+export const UPLOAD_URL = '/b6/upload'
